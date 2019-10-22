d738269efe7b02b3550215ef7aebd3d324bbc499
SaLiGiAisBack <36777927+SaLiGiAisBack@users.noreply.github.com>
2019-10-22 23:18:19 +0400

    Adaptive popup and fixes

--- App/scripts/main.js
+++ App/scripts/main.js
@@ -1,3 +1,25 @@
 function myFunction(x) {
     x.classList.toggle("change");
+    let menu = document.querySelector("#nav-menu");
+    menu.classList.toggle("menu-on");
 }
+// открыть по кнопке
+$('.btn-popup').click(function() {
+    $('.js-overlay-campaign').fadeIn();
+    $('.js-overlay-campaign').addClass('disabled');
+});
+
+// закрыть на крестик
+$('.close-popup').click(function() {
+    $('.js-overlay-campaign').fadeOut();
+
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+    var popup = $('.js-popup-campaign');
+    if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+        $('.js-overlay-campaign').fadeOut();
+
+    }
+});
